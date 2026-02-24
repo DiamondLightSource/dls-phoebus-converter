fed259a6160a9baaff1dcef9b2691b0efa69e7a6
Phil Smith <pts@observatorysciences.co.uk>
2026-02-24 17:22:14 +0000

    Add initial parsing of config yaml files
    The default arguments to main now work properly

--- converter.py
+++ converter.py
@@ -1,15 +1,37 @@
 import screen_converter
+import yaml
+from pathlib import Path
 
 
 class Converter:
-    def __init__(self):
-        self.screen_path_mapping: list[tuple]
-        self.get_config()
+    def __init__(self, config_file, test):
+        self.test = test
+        self.screen_path_mapping: list[tuple] = []
+        self.get_config(config_file)
 
-    def get_config(self, config_file) -> list[tuple]:
-        #get useful data out of json
-        self.screen_path_mapping = [("src/path", "dst/path")]
-        pass
+    def get_config(self, config_file):
+        # get useful data out of json
+        with open(config_file, "r") as file:
+            data = yaml.safe_load(file)
+            domain = data["meta_data"][0]["domain"]
+            print(f"Getting config data for domain: {domain}")
+            file_data = data["files"]
+            for path_data in file_data:
+                src_path = Path(path_data["src"])
+                dst_path = Path(path_data["dst"])
+                if src_path.is_dir():
+                    src_files = src_path.iterdir()
+                else:
+                    src_files = [src_path]
+
+                if self.test:
+                    # If in testing mode, output files to current directory/output
+                    dst_path = Path.cwd() / "output"
+                    dst_path.mkdir(exist_ok=True)
+                    
+                for src_file in src_files:
+                    if src_file.suffix == ".opi":
+                        self.screen_path_mapping.append((src_file, dst_path))
 
     def update_filepaths(self):
         # for line in xml file
@@ -32,15 +54,22 @@ class Converter:
     def convert(self):
         for screen_src, screen_dst in self.screen_path_mapping:
             # Convert .boy to .bob
-            screen_converter.main()
-            # Update filepaths
-            self.update_filepaths()
-            # Update macros
-            self.update_macros()
-            # Convert to techui-builder?
-            self.convert_to_techui_builder()
+            screen_converter.main(screen_src, screen_dst)
+            # # Update filepaths
+            # self.update_filepaths()
+            # # Update macros
+            # self.update_macros()
+            # # Convert to techui-builder?
+            # self.convert_to_techui_builder()
 
 
-def main(config_file):
-    converter = Converter(config_file)
+def main(
+    config_file=Path.cwd() / "config" / "front-ends.yaml",
+    test=True,
+):
+    converter = Converter(config_file, test)
     converter.convert()
+
+
+if __name__ == "__main__":
+    main()
