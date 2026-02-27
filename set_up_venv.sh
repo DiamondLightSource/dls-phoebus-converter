d41d66d03274d2d7a31e2ce7888cca81ec869a91
MichaelStubbings <michael.stubbings@diamond.ac.uk>
2026-02-27 16:55:56 +0000

    Add java/17 to the list of operations, required for phoebus use

--- set_up_venv.sh
+++ set_up_venv.sh
@@ -9,3 +9,6 @@ module unload python/${PYTHON_VERSION}
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# Required to use the inherent Phoebus converter
+module load java/17
