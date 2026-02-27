1b68ad70e6be05eea252353fad88b786500fa2c5
MichaelStubbings <michael.stubbings@diamond.ac.uk>
2026-02-27 16:12:29 +0000

    Add python venv setup script and requirements file

---
+++ set_up_venv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This application supports python 3.10 and above only.
+PYTHON_VERSION="3.11"
+
+module load python/${PYTHON_VERSION}
+python -m venv .venv
+module unload python/${PYTHON_VERSION}
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
