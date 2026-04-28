3da0636bd249532cf912c02a92791d7f1114cd41
Phil Smith <pts@observatorysciences.co.uk>
2026-04-28 18:37:02 +0100

    Remove uses of feTempIndicator.py

--- fe_special_case.py
+++ fe_special_case.py
@@ -1,8 +1,40 @@
 import logging
-
+import lxml.etree as ET
 import xmltodict
+import re
 
 logger = logging.getLogger("dls_phoebus_converter")
+
+
+def remove_feTempIndicator_script(bob_file, conversion):
+    """This script was being used to colour ProgressBars based on hihi and hi values.
+    Instead we now just use an alarm border which changes the border based on alarm.
+    Assuming hihi and hi are configured to generate Major and Minor alarms, the behaviour
+    is the same, and so we dont need this old rule which is broken in Phoebus anyway"""
+    
+    logger.info(f"Special case: Removing references to feTempIndicator.py from {bob_file}")
+
+    xml = xmltodict.unparse(conversion.all_phoebus_data)
+    xml_stripped = re.sub(r'<\?xml[^?]*\?>', '', xml)
+     # Parse the XML file
+    root = ET.fromstring(xml_stripped)
+    
+    # Find and remove all <script> elements which use feTempIndicator.py
+    for script in root.findall('.//script[@file="feTempIndicator.py"]'):
+        parent = script.getparent()
+        if parent is not None:
+            parent.remove(script)
+    
+    for script in root.findall('.//script[@file="common/plc/feTempIndicator.py"]'):
+        parent = script.getparent()
+        if parent is not None:
+            parent.remove(script)
+
+    # Write the modified XML back to our dict
+    as_dict = xmltodict.parse(ET.tostring(root))
+    conversion.all_phoebus_data = as_dict
+    if "widget" in as_dict["display"]:
+        conversion.widget_data = as_dict["display"]["widget"]
 
 
 def resize_absb_temps_fe22b(bob_file, conversion):
@@ -28,3 +60,7 @@ def run(bob_file, conversion):
 
     if "absb_temps_fe22b.bob" in str(bob_file):
         resize_absb_temps_fe22b(bob_file, conversion)
+
+    for name in ["FE24B.bob", "absb_pbpm_temps.bob", "absb_temps_fe22b.bob", "absb_temps.bob", "absb.bob"]:
+        if name in str(bob_file):
+            remove_feTempIndicator_script(bob_file, conversion)
