88e38d166cb7de912e07c873d015ae59d9aabe4f
Phil Smith <pts@observatorysciences.co.uk>
2026-08-14 18:21:48 +0000

    Refactor get_symbol_file_destinations

--- src/dls_phoebus_converter/post_converter.py
+++ src/dls_phoebus_converter/post_converter.py
@@ -313,42 +313,15 @@ def get_symbol_file_destinations(
     if sc is not None:
         # Decide where to put the symbol files based on the name of either the support
         # module we got the image from (priority) or our own support module.
-        if src_sm is not None:
-            if src_sm in ACC_UI_SUPPORT_MODULE_LIST:
-                output_file = sc.acc_ui_support_symbol_dst_part / src_sm / src_file.name
-                output_file_full = (
-                    sc.acc_ui_support_symbol_dst_full / src_sm / src_file.name
-                )
-            else:
-                output_file = (
-                    sc.domain_ui_support_symbol_dst_part / src_sm / src_file.name
-                )
-                output_file_full = (
-                    sc.domain_ui_support_symbol_dst_full / src_sm / src_file.name
-                )
+        sm = src_sm if src_sm is not None else oc.support_module_name
+
+        if sm in ACC_UI_SUPPORT_MODULE_LIST:
+            output_file = sc.acc_ui_support_symbol_dst_part / sm / src_file.name
+            output_file_full = sc.acc_ui_support_symbol_dst_full / sm / src_file.name
         else:
-            if oc.support_module_name in ACC_UI_SUPPORT_MODULE_LIST:
-                output_file = (
-                    sc.acc_ui_support_symbol_dst_part
-                    / oc.support_module_name
-                    / src_file.name
-                )
-                output_file_full = (
-                    sc.acc_ui_support_symbol_dst_full
-                    / oc.support_module_name
-                    / src_file.name
-                )
-            else:
-                output_file = (
-                    sc.domain_ui_support_symbol_dst_part
-                    / oc.support_module_name
-                    / src_file.name
-                )
-                output_file_full = (
-                    sc.domain_ui_support_symbol_dst_full
-                    / oc.support_module_name
-                    / src_file.name
-                )
+            output_file = sc.domain_ui_support_symbol_dst_part / sm / src_file.name
+            output_file_full = sc.domain_ui_support_symbol_dst_full / sm / src_file.name
+
     else:
         output_file = oc.dst_bob_dir_path / src_file.name
 
